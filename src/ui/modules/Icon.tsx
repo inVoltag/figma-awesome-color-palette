e58b69503ff0aa13dab4011a96459c973cf66838
@a-ng-d <a.grimaud@involt.io>
2024-12-06 17:39:07 +0100

    refactor: simplify Icon component SVG structure and update dimensions

--- src/ui/modules/Icon.tsx
+++ src/ui/modules/Icon.tsx
@@ -16,125 +16,17 @@ export default class Icon extends PureComponent<IconProps> {
         xmlns="http://www.w3.org/2000/svg"
       >
         <rect
-          x="8"
-          y="4"
-          width="112"
-          height="120"
-          rx="32"
-          fill="white"
-        />
-        <g clipPath="url(#clip0_1975_2917)">
-          <rect
-            x="47"
-            y="12"
-            width="34"
-            height="34"
-            fill="#BEDFE3"
-            stroke="#00272F"
-            strokeWidth="2"
-          />
-          <rect
-            x="47"
-            y="46"
-            width="34"
-            height="36"
-            fill="#E9F4F6"
-            stroke="#00272F"
-            strokeWidth="2"
-          />
-          <rect
-            x="47"
-            y="82"
-            width="34"
-            height="34"
-            fill="#0E8390"
-            stroke="#00272F"
-            strokeWidth="2"
-          />
-          <rect
-            x="16"
-            y="12"
-            width="31"
-            height="34"
-            fill="#003D47"
-            stroke="#00272F"
-            strokeWidth="2"
-          />
-          <rect
-            x="16"
-            y="46"
-            width="31"
-            height="36"
-            fill="#005460"
-            stroke="#00272F"
-            strokeWidth="2"
-          />
-          <rect
-            x="16"
-            y="82"
-            width="31"
-            height="34"
-            fill="#006C79"
-            stroke="#00272F"
-            strokeWidth="2"
-          />
-          <rect
-            x="81"
-            y="12"
-            width="31"
-            height="34"
-            fill="#93C9D0"
-            stroke="#00272F"
-            strokeWidth="2"
-          />
-          <rect
-            x="81"
-            y="46"
-            width="31"
-            height="36"
-            fill="#6AB2BC"
-            stroke="#00272F"
-            strokeWidth="2"
-          />
-          <rect
-            x="81"
-            y="82"
-            width="31"
-            height="34"
-            fill="#419BA7"
-            stroke="#00272F"
-            strokeWidth="2"
-          />
-        </g>
-        <rect
-          width="96"
-          height="104"
+          width="128"
+          height="128"
           rx="24"
-          transform="matrix(1 0 0 -1 16 116)"
-          stroke="#00272F"
-          strokeWidth="4"
-        />
-        <rect
-          x="40"
-          y="36"
-          width="48"
-          height="56"
-          rx="6"
           fill="#88EBF9"
-          stroke="#00272F"
-          strokeWidth="4"
         />
-        <defs>
-          <clipPath id="clip0_1975_2917">
-            <rect
-              width="96"
-              height="104"
-              rx="24"
-              transform="matrix(1 0 0 -1 16 116)"
-              fill="white"
-            />
-          </clipPath>
-        </defs>
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M41.7143 18.5C30.6546 18.5 21.75 27.5554 21.75 38.65V89.35C21.75 100.445 30.6546 109.5 41.7143 109.5H86.2857C97.3454 109.5 106.25 100.445 106.25 89.35V38.65C106.25 27.5554 97.3454 18.5 86.2857 18.5H41.7143ZM28.25 38.65C28.25 31.0774 34.3119 25 41.7143 25H49.375V41.25C46.6826 41.25 44.5 43.4326 44.5 46.125V49.375H28.25V38.65ZM44.5 52.625H28.25V75.375H44.5V52.625ZM44.5 78.625H28.25V89.35C28.25 96.9226 34.3119 103 41.7143 103H49.375V86.75C46.6826 86.75 44.5 84.5674 44.5 81.875V78.625ZM52.625 86.75V103H75.375V86.75H52.625ZM52.625 41.25H75.375V25H52.625V41.25ZM78.625 41.25C81.3174 41.25 83.5 43.4326 83.5 46.125V49.375H99.75V38.65C99.75 31.0774 93.6881 25 86.2857 25H78.625V41.25ZM83.5 52.625V75.375H99.75V52.625H83.5ZM83.5 78.625V81.875C83.5 84.5674 81.3174 86.75 78.625 86.75V103H86.2857C93.6881 103 99.75 96.9226 99.75 89.35V78.625H83.5Z"
+          fill="#00272F"
+        />
       </svg>
     )
   }
